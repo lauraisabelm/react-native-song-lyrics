978299bfa64801cf9b9e1864d65069866945a5b8
Laura Isabel Martínez <lauraisabelmartinez26@gmail.com>
2020-08-16 18:07:42 -0500

    Installed husky and made the necessary configurations to make husky, prettier and eslint work

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,6 +1,8 @@
 module.exports = {
-  bracketSpacing: false,
-  jsxBracketSameLine: true,
-  singleQuote: true,
   trailingComma: 'all',
+  tabWidth: 2,
+  singleQuote: true,
+  jsxBracketSameLine: false,
+  bracketSpacing: true,
+  printWidth: 100,
 };
